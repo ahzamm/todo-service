0e28100f77a43667a6e4f717dca63395a05ba3e2
ahzam <ahzamahmed6@gmail.com>
2023-07-12 13:27:22 +0500

    setup actix server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, App, web, Responder};
+use std::io;
+mod models;
+
+async fn status()-> impl Responder{ "{\"status\"}: \"UP\"}"}
+
+#[actix_rt::main]
+async fn main()-> io::Result<()> {
+
+    println!("The server is running at 127.0.0.1:8080");
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(status))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
