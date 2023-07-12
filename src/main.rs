10c9f1d6b3bd49806a7f058abb182498db416ed1
ahzam <ahzamahmed6@gmail.com>
2023-07-12 14:43:33 +0500

    make fixes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use actix_web::{HttpServer, App, web, Responder};
-use std::io;
 mod models;
 
-async fn status()-> impl Responder{ "{\"status\"}: \"UP\"}"}
+use actix_web::{Responder, HttpServer, App, web};
+use crate::models::Status;
+use std::io;
+
+
+
+async fn status() -> impl Responder {
+    web::HttpResponse::Ok()
+        .json(Status {status: "Up".to_string()})
+}
+
 
 #[actix_rt::main]
-async fn main()-> io::Result<()> {
+async fn main() -> io::Result<()> {
 
-    println!("The server is running at 127.0.0.1:8080");
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(status))
-    })
+    println!("Starting server at http://127.0.0.1:8080");
+
+    HttpServer::new(|| App::new() 
+        .route("/", web::get().to(status))
+    )
     .bind("127.0.0.1:8080")?
     .run()
     .await
